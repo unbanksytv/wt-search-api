804e118de7863967c763e9f2375b782b43f19fe0
Hynek Urban <hynek.urban@fragaria.cz>
2018-10-15 14:33:48 +0200

    refactor: Remove unused settings.

--- src/config/index.js
+++ src/config/index.js
@@ -3,8 +3,6 @@ const winston = require('winston');
 const env = process.env.WT_CONFIG || 'dev';
 
 module.exports = Object.assign({
-  port: 8080,
-  baseUrl: process.env.WT_API_BASE_URL || 'http://localhost:8080',
   logger: winston.createLogger({
     level: 'info',
     transports: [
