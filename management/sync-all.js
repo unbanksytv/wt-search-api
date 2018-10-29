6acffa53ba448a98222e6d3b9e5fd91a29399f0e
Hynek Urban <hynek.urban@fragaria.cz>
2018-10-29 09:54:55 +0100

    feat: Make sure the sync-all scripts terminates.

--- management/sync-all.js
+++ management/sync-all.js
@@ -1,8 +1,8 @@
-// Sync all hotels (store them to the DB and index them).
-// The script will probably hang due to an open DB connection.
-// You can end it with ctrl+C
-
+const { db } = require('../src/config');
+const { syncAll } = require('../src/services/crawler/queue-adapter');
 const server = require('../src/index'); // As a side effect, this will set up all the message listeners.
 
-const queue = require('../src/services/queue').get();
-queue.enqueue({ type: 'syncAll' });
+syncAll().finally(() => {
+  server.close();
+  db.destroy();
+});
