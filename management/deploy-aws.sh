f66bd9c5a45143344e68b9dde2c326cf6121f4fe
Jirka Chadima <chadima.jiri@gmail.com>
2018-12-11 11:33:25 +0100

    feat(docker): Drops the need for build args

--- management/deploy-aws.sh
+++ management/deploy-aws.sh
@@ -37,7 +37,7 @@ TASK_DEF="[{\"portMappings\": [{\"hostPort\": 0,\"protocol\": \"tcp\",\"containe
         \"value\": \"https://$WT_CONFIG-api.windingtree.com\"
       }
     ],
-    \"image\": \"029479441096.dkr.ecr.eu-west-1.amazonaws.com/wt-search-api:$LATEST_TAG-$ENVIRONMENT\",
+    \"image\": \"029479441096.dkr.ecr.eu-west-1.amazonaws.com/wt-search-api:$LATEST_TAG\",
     \"name\": \"wt-search-api\",
     \"memoryReservation\": 128,
     \"cpu\": 128
